c4aaf2ffdf201fdef8f5b2dc40a882a9d77515f1
nasimovi <43859472+nasimovi@users.noreply.github.com>
2021-04-04 16:48:01 -0400

    Update functions.R

--- scripts/functions.R
+++ scripts/functions.R
@@ -1,3 +1,4 @@
+# Principal component analysis
 pc_index <- function(dataIn=NULL,
                            varList=NULL){
   x <- dataIn[,varList]
@@ -6,6 +7,7 @@ pc_index <- function(dataIn=NULL,
   return(index)
 }
 
+# Model 1, no covariate adjustment
 reg_res_mod1<- function(outcome="", dataIn=NULL){
   Model1 <- lm(as.formula(paste0(outcome,"~","treatment")), data=dataIn)
   results <- tidy(Model1)
@@ -17,6 +19,7 @@ reg_res_mod1<- function(outcome="", dataIn=NULL){
   return(results[c("estimate","std.error","p.value", "N","outcome")])
 }  
 
+# Model 2, controlling for frequency of weekly FB usage
 reg_res_mod2<- function(outcome="", dataIn=NULL){
   covIn <- as.formula("~freq_usage")
   Model2 <- lm_lin(as.formula(paste0(outcome,"~","treatment")), covariates=covIn, data=dataIn)
@@ -28,6 +31,7 @@ reg_res_mod2<- function(outcome="", dataIn=NULL){
   return(results[c("estimate2","std.error2","p.value", "N","outcome")])
 }  
 
+# Model 3, controlling for a rich set of covariates
 reg_res_mod3 <- function(outcome="", dataIn=NULL){
   covIn <- as.formula("~freq_usage+as.factor(ethn_t)+gender+age+educ+employ1+imp_ethn+imp_cntry")
   Model3 <- lm_lin(as.formula(paste0(outcome,"~","treatment")), covariates=covIn, data=dataIn)
@@ -39,7 +43,7 @@ reg_res_mod3 <- function(outcome="", dataIn=NULL){
   return(results[c("estimate3","std.error3","p.value", "N","outcome")])
 }  
 
-
+# Full covariate specification (Model 3), excluding ethnicity dummies 
 reg_res_mod3_ethn<- function(outcome="", dataIn=NULL){
   covIn <- as.formula("~freq_usage+gender+age+educ+employ1+imp_cntry+imp_ethn")
   Model3 <- lm_lin(as.formula(paste0(outcome,"~","treatment")), covariates=covIn, data=dataIn)
@@ -51,14 +55,5 @@ reg_res_mod3_ethn<- function(outcome="", dataIn=NULL){
   return(results[c("estimate3","std.error3","p.value", "N","outcome")])
 }  
 
-
-interactions <- function(outcome="", covIn=NULL, dataIn=NULL){
-  Model3 <- lm_lin(as.formula(paste0(outcome,"~","treatment")), covariates=covIn, data=dataIn)
-  results <- tidy(Model3)
-  results <- results[results[,"term"]=="treatment",]
-  results$estimate3<-results$estimate/sd(dataIn[dataIn$treatment==0,outcome], na.rm=TRUE)
-  results$std.error3<-results$std.error/sd(dataIn[dataIn$treatment==0,outcome], na.rm=TRUE)
-  results$N <- Model3$nobs
-  return(results[c("estimate3","std.error3","p.value", "N","outcome")])}
 
 
